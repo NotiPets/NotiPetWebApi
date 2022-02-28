ed4ed636f4b1cde51aef4a7beb72744f618d0c19
Diego Grau <diegograu257@gmail.com>
2022-02-28 12:05:22 -0400

    Update references and vaccine model
    Vaccine model created and references between layers updated

--- Notipet.Domain/Vaccine.cs
+++ Notipet.Domain/Vaccine.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Notipet.Domain
 {
-    internal class Vaccine
+    public  class Vaccine
     {
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column("PetTypeId")]
+        public PetType? PetType { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string? VaccineName { get; set; }
+
+        [Required]
+        [Column("BusinessId")]
+        public Business? Business { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
     }
 }
