5786c48b12574ef7bc6f096bff1b5a93c6f32791
Waldo Ruiz <waldo.omaet6446@gmail.com>
2022-02-28 22:30:22 -0400

    Minor changes

--- Notipet.Web/Program.cs
+++ Notipet.Web/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using Notipet.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NotiPetBdContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Utilities/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Constants
+    {
+        public static class JsendStatus
+        {
+            public const string Success = "success";
+            public const string Fail = "fail";
+            public const string Error = "error";
+        }
+    }
+}
