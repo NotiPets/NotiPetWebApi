9caea12e438b69b8fdab4c24c170b8d71f684e76
Waldo Ruiz <waldo.omaet6446@gmail.com>
2022-03-06 21:06:02 -0400

    Eager loading added to login

--- Notipet.Web/Controllers/LogInController.cs
+++ Notipet.Web/Controllers/LogInController.cs
@@ -36,7 +36,10 @@ namespace Notipet.Web.Controllers
         public async Task<ActionResult<UserRole>> LogIn(LoginDto login)
         {
             login.Password = Methods.ComputeSha256Hash(login.Password);
-            var userRole = await _context.UserRoles.Where(x => x.Username == login.Username && x.Password == login.Password).FirstOrDefaultAsync();
+            var userRole = await _context.UserRoles.Where(x => x.Username == login.Username && x.Password == login.Password)
+                .Include(x => x.User)
+                .Include(x => x.Business)
+                .FirstOrDefaultAsync();
             if (userRole != null)
             {
                 return Ok(new JsendSuccess(new
