526b959e15018708a576714d2bdc61e90a0a646e
Diego Grau <diegograu257@gmail.com>
2022-03-14 11:40:15 -0400

    LogIn works

--- Notipet.Web/DTO/LoginDto.cs
+++ Notipet.Web/DTO/LoginDto.cs
@@ -14,6 +14,5 @@ namespace Notipet.Web.DTO
         public string? Email { get; set; }
         public string? Username { get; set; }
         public string? BusinessId { get; set; }
-        public User? User { get; set; }
     }
 }
