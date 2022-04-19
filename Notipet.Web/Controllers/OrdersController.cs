a93b4bd6b89332c6f0fea1012e34d633e84ad32a
Waldo Ruiz <waldo.omaet6446@gmail.com>
2022-04-19 10:29:12 -0400

    Order controller

---
+++ Notipet.Web/Controllers/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notipet.Data;
+using Notipet.Domain;
+using Notipet.Web.DataWrapper;
+using Utilities;
+
+namespace Notipet.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly NotiPetBdContext _context;
+
+        public OrdersController(NotiPetBdContext context)
+        {
+            _context = context;
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("CancelOrder/{id}")]
+        public async Task<ActionResult<JsendWrapper>> PutOrder(Guid id)
+        {
+            try
+            {
+                var order = await _context.Orders.FindAsync(id);
+                if (order == null)
+                {
+                    return NotFound(new JsendFail(new { order = "order not found" }));
+                }
+                order.OrderStatus = OrderStatusId.Cancelled;
+                _context.Entry(order).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(new JsendSuccess());
+            }
+            catch (Exception e)
+            {
+                string error = $"{e.Message}\n{e.InnerException}\n{e.StackTrace}";
+                if (Methods.IsDevelopment())
+                {
+                    return StatusCode(500, new JsendError(error));
+                }
+                Console.WriteLine(error);
+                return StatusCode(500, new JsendError(Constants.ControllerTextResponse.Error));
+            }
+        }
+    }
+}
