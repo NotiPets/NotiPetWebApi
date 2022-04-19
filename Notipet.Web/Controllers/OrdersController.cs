3e0eb44879b98dbad6df8d9072b00decd02ec2d5
Waldo Ruiz <waldo.omaet6446@gmail.com>
2022-04-19 10:54:21 -0400

    Useful endpoints for orders

--- Notipet.Web/Controllers/OrdersController.cs
+++ Notipet.Web/Controllers/OrdersController.cs
@@ -24,22 +24,30 @@ namespace Notipet.Web.Controllers
             _context = context;
         }
 
-        // PUT: api/Orders/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpGet]
+        public async Task<ActionResult<JsendWrapper>> AllOrders() => Ok(new JsendSuccess(await _context.Orders.ToListAsync()));
+
+        [HttpGet("ByUser/{userId}")]
+        public async Task<ActionResult<JsendWrapper>> OrdersByUser(Guid userId) => Ok(new JsendSuccess(await _context.Orders.Where(x => x.UserId == userId).ToListAsync()));
+
+        [HttpGet("ByBusiness/{businessId}")]
+        public async Task<ActionResult<JsendWrapper>> OrdersByBusiness(int businessId) => Ok(new JsendSuccess(await _context.Sales.Where(x => x.BusinessId == businessId).Select(x => x.Orders).ToListAsync()));
+
+
         [HttpPut("CancelOrder/{id}")]
         public async Task<ActionResult<JsendWrapper>> PutOrder(Guid id)
         {
             try
             {
-                var order = await _context.Orders.FindAsync(id);
-                if (order == null)
+                if (await _context.Orders.Where(x => x.Id == id).AnyAsync())
+                {
+                    await UpdateOrderState(id, OrderStatusId.Cancelled);
+                    return Ok(new JsendSuccess());
+                }
+                else
                 {
                     return NotFound(new JsendFail(new { order = "order not found" }));
                 }
-                order.OrderStatus = OrderStatusId.Cancelled;
-                _context.Entry(order).State = EntityState.Modified;
-                await _context.SaveChangesAsync();
-                return Ok(new JsendSuccess());
             }
             catch (Exception e)
             {
@@ -51,6 +59,41 @@ namespace Notipet.Web.Controllers
                 Console.WriteLine(error);
                 return StatusCode(500, new JsendError(Constants.ControllerTextResponse.Error));
             }
+        }
+
+        [HttpPut("Status/{id}/{status}")]
+        public async Task<ActionResult<JsendWrapper>> PutOrder(Guid id, OrderStatusId status)
+        {
+            try
+            {
+                if (await _context.Orders.Where(x => x.Id == id).AnyAsync())
+                {
+                    await UpdateOrderState(id, status);
+                    return Ok(new JsendSuccess());
+                }
+                else
+                {
+                    return NotFound(new JsendFail(new { order = "order not found" }));
+                }
+            }
+            catch (Exception e)
+            {
+                string error = $"{e.Message}\n{e.InnerException}\n{e.StackTrace}";
+                if (Methods.IsDevelopment())
+                {
+                    return StatusCode(500, new JsendError(error));
+                }
+                Console.WriteLine(error);
+                return StatusCode(500, new JsendError(Constants.ControllerTextResponse.Error));
+            }
+        }
+
+        private async Task UpdateOrderState(Guid id, OrderStatusId status)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            order.OrderStatus = OrderStatusId.Cancelled;
+            _context.Entry(order).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
     }
 }
